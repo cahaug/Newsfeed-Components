97b5343724afcb7c4a529a50eea8992cd794e8e0
cahaug <caleahaug@gmail.com>
2019-05-07 18:52:36 -0700

    menu refactored, working forwards and back

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,22 @@
-
+let menuActive
 const toggleMenu = () => {
 // Toggle the "menu--open" class on your menu refence. 
-menu.classList.toggle('menu--open');
+// menu.classList.toggle('menu--open');
+if (!menuActive) {
+    menu.classList.toggle('menu--open');
+    TweenMax.to(".menu", .6, {
+      left: 0,
+    })
+    menuActive = true;
+  } else if (menuActive) {
+    TweenMax.to(".menu", .6, {
+      left: -600,
+    })
+    menuActive = false;
+    setTimeout(() => {
+      menu.classList.toggle('menu--open');
+    }, 200)
+  }
 };
 
 // Start Here: Create a reference to the ".menu" class
@@ -11,6 +26,8 @@ console.log(menu);
 const menuButton = document.querySelector('.menu-button');
 console.log(menuButton);
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', () => {toggleMenu(); TweenMax.from(".menu", 1, {x:-800}); TweenMax.to(".menu", 3, {x:0});});
+// menuButton.addEventListener('click', () => {toggleMenu(); TweenMax.from(".menu", 1, {x:-800}); TweenMax.to(".menu", 3, {x:0});});
+menuButton.addEventListener('click', () => {toggleMenu();});
+
 // const slideIn = TweenMax.from(".menu", 3, {x:-800});
 // menuButton.addEventListener('click', () => {slideIn});
